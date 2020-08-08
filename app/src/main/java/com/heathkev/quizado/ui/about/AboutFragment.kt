43a091667f718a2834909f76bc7656aafee397ed
heathkev <kevintuazon45@gmail.com>
2020-08-09 02:07:43 +0800

    update AcknowledgementFragment and AboutFragment

--- app/src/main/java/com/heathkev/quizado/ui/about/AboutFragment.kt
+++ app/src/main/java/com/heathkev/quizado/ui/about/AboutFragment.kt
@@ -5,19 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.heathkev.quizado.MainNavigationFragment
 import com.heathkev.quizado.R
+import com.heathkev.quizado.utils.doOnApplyWindowInsets
+import kotlinx.android.synthetic.main.fragment_settings.*
 
-/**
- * A simple [Fragment] subclass.
- */
-class AboutFragment : Fragment() {
+class AboutFragment : MainNavigationFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.doOnApplyWindowInsets { _, insets, _ ->
+            status_bar.run {
+                layoutParams.height = insets.systemWindowInsetTop
+                isVisible = layoutParams.height > 0
+                requestLayout()
+            }
+        }
+    }
 }
--- app/src/main/java/com/heathkev/quizado/ui/acknowledgement/AcknowledgementFragment.kt
+++ app/src/main/java/com/heathkev/quizado/ui/acknowledgement/AcknowledgementFragment.kt
@@ -5,15 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import com.heathkev.quizado.MainNavigationFragment
 import com.heathkev.quizado.R
+import com.heathkev.quizado.utils.doOnApplyWindowInsets
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 
-class AcknowledgementFragment : Fragment() {
+class AcknowledgementFragment : MainNavigationFragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_acknowledgement, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.doOnApplyWindowInsets { _, insets, _ ->
+            status_bar.run {
+                layoutParams.height = insets.systemWindowInsetTop
+                isVisible = layoutParams.height > 0
+                requestLayout()
+            }
+        }
     }
 }
