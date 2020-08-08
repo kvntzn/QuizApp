444b3609e2f3e3a4a69117c4a652e8be1f00d422
heathkev <kevintuazon45@gmail.com>
2020-08-09 02:31:56 +0800

    Fixed navigations

---
+++ app/src/main/java/com/heathkev/quizado/DispatchInsetsNavHostFragment.kt
@@ -0,0 +1,24 @@
+package com.heathkev.quizado
+
+import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.forEach
+import androidx.navigation.fragment.NavHostFragment
+
+class DispatchInsetsNavHostFragment : NavHostFragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.setOnApplyWindowInsetsListener { v, insets ->
+            // During fragment transitions, multiple fragment's view hierarchies can be added at the
+            // same time. If one consumes window insets, the other might not be layed out properly.
+            // To workaround that, make sure we dispatch the insets to all children, regardless of
+            // how they are consumed.
+            (v as? ViewGroup)?.forEach { child ->
+                child.dispatchApplyWindowInsets(insets)
+            }
+            insets
+        }
+    }
+}
