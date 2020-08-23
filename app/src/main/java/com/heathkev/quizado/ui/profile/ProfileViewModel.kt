0e9ea5b9b0d7a198fb79ac84382120e557c2c1e4
heathkev <kevintuazon45@gmail.com>
2020-08-23 13:54:05 +0800

    update list

--- app/src/main/java/com/heathkev/quizado/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/com/heathkev/quizado/ui/profile/ProfileViewModel.kt
@@ -1,5 +1,6 @@
 package com.heathkev.quizado.ui.profile
 
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
@@ -8,25 +9,35 @@ import com.google.firebase.firestore.ktx.toObject
 import com.heathkev.quizado.data.Result
 import com.heathkev.quizado.data.User
 import com.heathkev.quizado.firebase.FirebaseRepository
+import com.heathkev.quizado.firebase.FirebaseUserLiveData
 import kotlinx.coroutines.*
 
 private const val TAG = "ProfileViewModel"
-
-class ProfileViewModel(private val currentUser: User) : ViewModel() {
-
-    private val firebaseRepository = FirebaseRepository()
+class ProfileViewModel @ViewModelInject constructor (
+    private val firebaseRepository: FirebaseRepository,
+    private val firebaseUser: FirebaseUserLiveData
+) : ViewModel() {
     private val NO_RECORD: Int = 0
 
     private var viewModelJob = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + viewModelJob)
 
-    private val _user = MutableLiveData<User>()
     private val _quizzesNumber = MutableLiveData<Int>()
     private val _passedNumber = MutableLiveData<Int>()
     private val _failedNumber = MutableLiveData<Int>()
 
-    val user: LiveData<User>
-        get() = _user
+    val user: LiveData<User> = Transformations.map(firebaseUser){ user ->
+        if(user != null){
+            User(
+                user.uid,
+                user.displayName,
+                user.photoUrl,
+                user.email
+            )
+        }else{
+            User()
+        }
+    }
 
     val quizzesNumber: LiveData<String> = Transformations.map(_quizzesNumber) {
         it.toString()
@@ -40,12 +51,7 @@ class ProfileViewModel(private val currentUser: User) : ViewModel() {
         it.toString()
     }
 
-    init {
-        _user.value = currentUser
-        getResult()
-    }
-
-    private fun getResult() {
+    fun getResult(currentUser: User) {
         uiScope.launch {
             val value = withContext(Dispatchers.IO) {
                 firebaseRepository.getResultsByUserId(currentUser.userId)
